308813dde204d25db58c970c18c6bcd809cbf4ae
User <aleksakovacevicc@gmail.com>
2025-02-06 17:55:11 +0100

    configuration logger to write in the file and in the console

--- boot/configurelogger.go
+++ boot/configurelogger.go
@@ -1,21 +1,26 @@
 package boot
 
 import (
+	"io"
 	"log"
+	"os"
 
 	"gopkg.in/natefinch/lumberjack.v2"
 )
 
 // configureLogger sets up the logger with file rotation and log levels
 func configureLogger() {
-	// Configure log rotation using Lumberjack
-	log.SetOutput(&lumberjack.Logger{
+	fileLogger := &lumberjack.Logger{
 		Filename:   "user_files/logs/system.log", // Log file name
 		MaxSize:    10,                           // Max size in MB before rotation
 		MaxBackups: 5,                            // Max number of old log files to retain
 		MaxAge:     30,                           // Max age in days to retain old logs
 		Compress:   true,                         // Compress rotated files
-	})
-	log.Println("INFO: Logger configured successfully")
+	}
 
+	// Create a multi-writer to write to both console and file
+	multiWriter := io.MultiWriter(os.Stdout, fileLogger)
+	log.SetOutput(multiWriter)
+
+	log.Println("INFO: Logger configured successfully")
 }
